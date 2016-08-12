2e388e4d7041e73f40544d331cd9c687d1036e70
Dongyang.Cai <yangguangzaidongji@hotmail.com>
2016-08-12 11:55:41 +0800

    Init project.

---
+++ Gemini/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Gemini
+//
+//  Created by Dongyang.Cai on 8/11/16.
+//  Copyright © 2016 Dongyang.Cai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
